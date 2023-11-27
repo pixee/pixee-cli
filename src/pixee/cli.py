c269797faab455766abb6ce2a232962fa312eb41
Daniel D'Avella <dan.davella@pixee.ai>
2023-11-27 11:28:58 -0500

    Basic CLI prototype working

--- src/pixee/cli.py
+++ src/pixee/cli.py
@@ -1,7 +1,28 @@
 from argparse import ArgumentParser
 
+from .logo import logo2 as logo
+
 def main():
-    print('Hello, World!')
+    print(logo)
+
+    parser = ArgumentParser(description='Pixee CLI')
+    subparsers = parser.add_subparsers(dest="command", help='Commands')
+
+    fix = subparsers.add_parser('fix', help='Find and fix vulnerabilities')
+    fix.add_argument('path', nargs='?', help='Path to the project to fix')
+
+    subparsers.add_parser('triage', help='Triage findings')
+    subparsers.add_parser('remediate', help='Fix vulnerabilities from external tools')
+
+
+    args = parser.parse_args()
+
+    if not args.command:
+        parser.print_help()
+        return
+
+    if args.command == "fix" and not args.path:
+        return
 
 if __name__ == '__main__':
     main()
