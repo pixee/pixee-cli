5ba65d507b121f2f97f6641c2dd0a3ee00e353a4
Daniel D'Avella <dan.davella@pixee.ai>
2023-11-28 15:52:57 -0500

    Add dependency on codemodder package

--- Formula/homebrew/pixee.rb
+++ Formula/homebrew/pixee.rb
@@ -9,4 +9,9 @@ class Pixee < Formula
   depends_on "python@3.11"
   depends_on "openjdk@17"
 
+  resource "codemodder" do
+    url "https://files.pythonhosted.org/packages/18/ae/ce5f2d1d6f632f728e173be52212fe29d601a3a3146c35cb725217d7190f/codemodder-0.64.5.tar.gz"
+    sha256 "752f0fc447b26bd1b913ed6df508bc8b7b21ec1159086c0107eecc48e9b1c55e"
+  end
+
 end
