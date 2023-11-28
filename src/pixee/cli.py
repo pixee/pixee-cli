44d18827babbfba3482125004f39dfbc99e2c8db
Daniel D'Avella <dan.davella@pixee.ai>
2023-11-28 11:33:49 -0500

    Run Python codemodder

--- src/pixee/cli.py
+++ src/pixee/cli.py
@@ -1,10 +1,33 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
+import os
+import subprocess
+
+from prompt_toolkit import prompt
+from prompt_toolkit.completion.filesystem import PathCompleter
+from rich.console import Console
 
 from .logo import logo2 as logo
 
 
+def run_fix(console: Console, args: Namespace):
+    console.print("Welcome to Pixee!", style="bold")
+    console.print("Let's find and fix vulnerabilities in your project.", style="bold")
+    if not args.path:
+        args.path = prompt(
+            "Path to the project to fix: ",
+            complete_while_typing=True,
+            complete_in_thread=True,
+            completer=PathCompleter(),
+            default=os.getcwd(),
+        )
+
+    output_path = "results.codetf.json"
+    subprocess.run(["codemodder", args.path, "--output", output_path], check=True)
+
+
 def main():
-    print(logo)
+    console = Console()
+    console.print(logo, style="bold cyan")
 
     parser = ArgumentParser(description="Pixee CLI")
     subparsers = parser.add_subparsers(dest="command", help="Commands")
@@ -21,8 +44,9 @@ def main():
         parser.print_help()
         return
 
-    if args.command == "fix" and not args.path:
-        return
+    match args.command:
+        case "fix":
+            run_fix(console, args)
 
 
 if __name__ == "__main__":
