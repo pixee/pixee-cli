9510c60aa851ffea1b8959b29da63e483f136bf4
Matt Austin <matt@m-austin.com>
2024-02-27 10:32:29 -0800

    lint cleanup

--- ci/gitlab/pipeline.py
+++ ci/gitlab/pipeline.py
@@ -9,22 +9,25 @@ import base64
 
 # GitLab settings
 gitlab_url = "https://gitlab.com"  # Replace with your GitLab URL
-api_token = os.environ.get('API_TOKEN')
-project_id = os.environ.get('CI_MERGE_REQUEST_PROJECT_ID')
-source_branch = os.environ.get('CI_MERGE_REQUEST_SOURCE_BRANCH_NAME')
-new_branch_name = "pixee_" + str(random.randint(0, 1000))  # Replace with the desired new branch name
-merge_id = os.environ.get('CI_MERGE_REQUEST_IID')
+api_token = os.environ.get("API_TOKEN")
+project_id = os.environ.get("CI_MERGE_REQUEST_PROJECT_ID")
+source_branch = os.environ.get("CI_MERGE_REQUEST_SOURCE_BRANCH_NAME")
+new_branch_name = "pixee_" + str(
+    random.randint(0, 1000)
+)  # Replace with the desired new branch name
+merge_id = os.environ.get("CI_MERGE_REQUEST_IID")
 
-source_title = os.environ.get('CI_MERGE_REQUEST_TITLE')
+source_title = os.environ.get("CI_MERGE_REQUEST_TITLE")
+
 
 def main():
     for name, value in os.environ.items():
         print("{0}: {1}".format(name, value))
-    
+
     filename = str(sys.argv[1])
-    #working_dir = os.path.dirname(os.path.abspath(filename))
-    
-    with open(filename, 'r', encoding="utf-8") as file:
+    # working_dir = os.path.dirname(os.path.abspath(filename))
+
+    with open(filename, "r", encoding="utf-8") as file:
         data = json.load(file)
 
     # Initialize GitLab client
@@ -32,47 +35,46 @@ def main():
 
     # Create a new branch based on an existing branch
     project = gl.projects.get(project_id)
-    branch = project.branches.create(
-        {"branch": new_branch_name, "ref": source_branch}
-    )
+    branch = project.branches.create({"branch": new_branch_name, "ref": source_branch})
 
     print(f"Created a new branch: {branch.name}")
-    
+
     description = ""
-    
+
     for result in data["results"]:
         if len(result["changeset"]):
             print(result["summary"])
-        
-            description = description + f"""
+
+            description = (
+                description
+                + f"""
 ## {result["summary"]}
 
 {result["description"]}
 """
+            )
             print(description)
-    
+
             for entry in result["changeset"]:
                 try:
                     print(entry["path"])
-        
+
                     file = project.files.get(entry["path"], ref=new_branch_name)
                     original_file_content = base64.b64decode(file.content).decode()
-        
+
                     diff = [x for x in whatthepatch.parse_patch(entry["diff"])]
                     diff = diff[0]
-        
-                    new_file = whatthepatch.apply_diff(diff, original_file_content, use_patch=True)
-                    new_file = "\n".join(new_file[0])
-        
-        
-                    file.content = new_file
-                    file.save(
-                        branch=new_branch_name,
-                        commit_message=result["summary"]
+
+                    new_file = whatthepatch.apply_diff(
+                        diff, original_file_content, use_patch=True
                     )
+                    new_file = "\n".join(new_file[0])
+
+                    file.content = new_file
+                    file.save(branch=new_branch_name, commit_message=result["summary"])
                     print(file)
                 except Exception as e:
-                    print("The error is: ",e)
+                    print("The error is: ", e)
 
     new_mr = project.mergerequests.create(
         {
@@ -88,9 +90,14 @@ def main():
     merge_request = project.mergerequests.get(merge_id)
 
     # Add a comment to the merge request
-    merge_request.notes.create({'body': f"Pixee has created some suggestions in: [Hardening Suggestions for {source_branch}]({new_mr.web_url})" })
-            
+    merge_request.notes.create(
+        {
+            "body": f"Pixee has created some suggestions in: [Hardening Suggestions for {source_branch}]({new_mr.web_url})"
+        }
+    )
 
-        #print(commit)
+    # print(commit)
+
+
 if __name__ == "__main__":
     main()
